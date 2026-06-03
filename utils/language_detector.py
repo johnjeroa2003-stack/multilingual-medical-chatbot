ddf59e8e5823e87d5f0d2294516cde3a81aefc6e
Your Name <yourmail@gmail.com>
2026-06-03 12:02:29 +0530

    Fixed language detection

--- utils/language_detector.py
+++ utils/language_detector.py
@@ -5,8 +5,11 @@ def detect_language(text):
     try:
         language = detect(text)
 
-        # Force English for normal English sentences
-        if language not in ["ta", "hi", "fr", "es", "de"]:
+        supported_languages = [
+            "en", "ta", "hi", "fr", "es", "de"
+        ]
+
+        if language not in supported_languages:
             return "en"
 
         return language
